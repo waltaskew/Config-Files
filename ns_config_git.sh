f5b39b0f130692794d2043e41302e1c462d57950
waskew <waskew@narrativescience.com>
2012-08-14 20:26:16 +0000

    set vim and vimdiff as my editor and difftool

--- ns_config_git.sh
+++ ns_config_git.sh
@@ -1,3 +1,8 @@
-git config --global user.name "waskew"
-git config --global user.email "waskew@narrativescience.com"
+git config --global user.name 'waskew'
+git config --global user.email waskew@narrativescience.com
+git config --global core.editor vim
 git config --global color.ui true
+
+git config --global diff.tool vimdiff
+git config --global difftool.prompt false
+git config --global alias.vimdiff difftool
