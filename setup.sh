121cc7a8efb616667d0c9fc3e15de71404d38314
waltaskew <waltaskew@gmail.com>
2014-12-31 07:58:00 -0800

    add setup script

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+for file in .emacs .emacs.d .gitconfig  .vim .vimrc
+do
+  ln -s $DIR/$file  ~/$file
+done
+
+if [ -e ~/.profile ]
+then
+    echo '' >> ~/.profile
+    cat $DIR/.profile >> ~/.profile
+else
+    ln -s $DIR/.profile ~/.profile
+fi
