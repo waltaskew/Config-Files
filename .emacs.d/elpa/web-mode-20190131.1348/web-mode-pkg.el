7e3d6a709f44b36adb00045e7b24a0a0e1cb55d7
waltaskew <walt@picnichealth.com>
2020-09-25 10:18:24 -0700

    package upgrades

--- .emacs.d/elpa/web-mode-20190131.1348/web-mode-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "web-mode" "20190131.1348" "major mode for editing web templates" '((emacs "23.1")) :commit "70fcfb6e127a466ffb8ab6674f62cf40912b8a9d" :keywords '("languages") :authors '(("Fran\303\247ois-Xavier Bois <fxbois AT Google Mail Service>")) :maintainer '("Fran\303\247ois-Xavier Bois") :url "http://web-mode.org")
